927519ad92dcf83e7c5c8b6a4c17c1ae5db6d2c6
Gabriel Scherer <gabriel.scherer@gmail.com>
2023-08-08 14:32:34 +0200

    benchmarks: avoid float boxing in the benchmark harness

--- src/test_sincos.ml
+++ src/test_sincos.ml
@@ -1,16 +1,18 @@
-
-module A = Array
+module A = Float.Array
 
 let pi = 4.0 *. atan 1.0
 
 let main () =
-  let n = 30_000_000 in
-  let alphas = A.init n (fun i -> (float i) *. pi /. (float n)) in
+  let datalen = 1000 in
+  let alphas = A.init datalen (fun i -> float i *. pi /. float datalen) in
+  let n = 30_000 in
   let t0 = Unix.gettimeofday () in
-  let res = Sincos.{ sin = 0.0; cos = 0.0 } in  
-  for i = 0 to n - 1 do
-    let a = A.unsafe_get alphas i in
-    Sincos.sincos a res
+  let res = Sincos.{ sin = 0.0; cos = 0.0 } in
+  for _ = 1 to n do
+    for i = 0 to datalen - 1 do
+      let x = A.unsafe_get alphas i in
+      Sincos.sincos x res;
+    done;
   done;
   let t1 = Unix.gettimeofday () in
   Printf.printf "sincos_: %f\n" (t1 -. t0)
