2a9a7135f211327e9e8544aad62a05e9f3b69dc4
Francois Berenger <unixjunkie@sdf.org>
2023-08-08 13:48:08 +0900

    Test_no_regr: print by how much they differ, if so

--- src/test_no_regr.ml
+++ src/test_no_regr.ml
@@ -9,7 +9,16 @@ let main () =
   let alphas = A.init n (fun i -> ((float i) *. two_pi) /. (float n)) in
   let sin_cos = A.map (fun a -> sin a, cos a) alphas in
   let sincos = A.map Sincos.sincos alphas in
-  (* check they compute the exact same thing *)
-  assert(sin_cos = sincos)
-  
+  (* check they compute the same thing *)
+  for i = 0 to n - 1 do
+    let sin, cos = sin_cos.(i) in
+    let sin', cos' = sincos.(i) in
+    let eps = sin -. sin' in
+    let eps' = cos -. cos' in
+    if eps <> 0.0 then
+      Printf.eprintf "sin diff of %g at %d\n" eps i;
+    if eps' <> 0.0 then
+      Printf.eprintf "cos diff of %g at %d\n" eps' i
+  done
+
 let () = main ()
