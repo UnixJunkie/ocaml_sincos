20f5593094d6ec67e52c9508d2cff689a0d02f71
Francois Berenger <unixjunkie@sdf.org>
2023-08-16 18:39:10 +0900

    @yallop's suggestion

--- src/sincos.c
+++ src/sincos.c
@@ -6,51 +6,7 @@
 #include "caml/alloc.h"
 #include "caml/memory.h"
 
-/* CAMLprim value caml_modf_float(value f) */
-/* { */
-/*   CAMLparam0 (); */
-/*   CAMLlocal2 (quo, rem); */
-/*   value res; */
-/*   double frem; */
-
-/*   quo = caml_copy_double(modf (Double_val(f), &frem)); */
-/*   rem = caml_copy_double(frem); */
-/*   res = caml_alloc_small(2, 0); */
-/*   Field(res, 0) = quo; */
-/*   Field(res, 1) = rem; */
-/*   CAMLreturn (res); */
-/* } */
-
-/* CAMLprim value caml_sincos_float(value f) */
-/* { */
-/*   CAMLparam0 (); */
-/*   CAMLlocal2 (sine, cosine); */
-/*   value res; */
-/*   double x, y; */
-
-/*   sincos(Double_val(f), &x, &y); */
-
-/*   sine = caml_copy_double(x); */
-/*   cosine = caml_copy_double(y); */
-/*   res = caml_alloc_small(2, 0); */
-/*   Field(res, 0) = sine; */
-/*   Field(res, 1) = cosine; */
-/*   CAMLreturn (res); */
-/* } */
-
-/* trying to store the result in a mutable floats struct */
 CAMLprim value caml_sincos_float(double f, value res) {
-  double x;
-  double y;
-
-  sincos(f, &x, &y);
-
-  Store_double_flat_field(res, 0, x);
-  Store_double_flat_field(res, 1, y);
-
-  return (Val_unit);
-}
-
-CAMLprim value caml_sincos_float_boxed(value f, value res) {
-  return caml_sincos_float(Double_val(f), res);
+  sincos(f, (double*)res, 1+(double*)res);
+  return Val_unit;
 }
--- src/sincos.ml
+++ src/sincos.ml
@@ -5,5 +5,4 @@
 type sincos_res = { mutable sin: float ;
                     mutable cos: float }
 
-external sincos : (float [@unboxed]) -> sincos_res -> unit = "caml_sincos_float_boxed" "caml_sincos_float"
-  [@@noalloc]
+external sincos : float -> sincos_res -> unit = "caml_sincos_float"
